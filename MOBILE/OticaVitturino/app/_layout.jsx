a6323b36482c4e325cc658566579877a4a04fe19
caiovtech <caiovtech@outlook.com>
2026-04-15 15:33:43 -0300

    Add: Added more font weight options for "Poppins"

--- MOBILE/OticaVitturino/app/_layout.jsx
+++ MOBILE/OticaVitturino/app/_layout.jsx
@@ -1,9 +1,11 @@
 import { Slot } from 'expo-router';
-import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 export default function Layout() {
   let [fontsLoaded] = useFonts({
     PoppinsRegular: Poppins_400Regular,
+    PoppinsMedium: Poppins_500Medium,
+    PoppinsSemiBold: Poppins_600SemiBold,
     PoppinsBold: Poppins_700Bold,
   });
 
