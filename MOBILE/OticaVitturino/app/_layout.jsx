32c92d0e0f3c4248af7ffd9ae20df5483b62d276
caiovtech <caiovtech@outlook.com>
2026-04-15 15:01:38 -0300

    Add: Font "Poppins" added to the project

---
+++ MOBILE/OticaVitturino/app/_layout.jsx
@@ -0,0 +1,15 @@
+import { Slot } from 'expo-router';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+export default function Layout() {
+  let [fontsLoaded] = useFonts({
+    PoppinsRegular: Poppins_400Regular,
+    PoppinsBold: Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; 
+  }
+
+  return <Slot />;
+}
