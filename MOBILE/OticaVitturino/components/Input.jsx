27f7c63679ff5f2f67cf5ed7505bb2ef19a1e5e3
caiovtech <caiovtech@outlook.com>
2026-04-15 15:36:40 -0300

    Mod: Input component updated

--- MOBILE/OticaVitturino/components/Input.jsx
+++ MOBILE/OticaVitturino/components/Input.jsx
@@ -9,9 +9,11 @@ export function Input({ ...rest }) {
 const styles = StyleSheet.create({
     input: {
         width: "100%",
-        height: 68,
+        height: 64,
         paddingLeft: 12,
-        borderRadius: 14,
+        borderRadius: 10,
         backgroundColor: "#FFFFFF",
+        fontFamily: "PoppinsRegular",
+        textAlign: "center",
     }
 })
