49531e155b2cc1748b1f7a1ffc47171698f859b6
caiovtech <caiovtech@outlook.com>
2026-04-18 15:42:31 -0300

    Mod: Button adjusted to receive custom stylizations

--- MOBILE/OticaVitturino/components/Button.jsx
+++ MOBILE/OticaVitturino/components/Button.jsx
@@ -1,9 +1,9 @@
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
-export function Button({ title, onPress, ...rest }) {
+export function Button({ title, onPress, style, textStyle, ...rest }) {
     return (
-        <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={onPress}>
-            <Text style={styles.buttonText}>{title}</Text>
+        <TouchableOpacity style={[styles.button, style]} activeOpacity={0.6} onPress={onPress}>
+            <Text style={[styles.buttonText, textStyle]}>{title}</Text>
         </TouchableOpacity>
     )
 }
