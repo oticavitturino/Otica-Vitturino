29ad86b1df7b2533896b41e129037e7566b234fb
caiovtech <caiovtech@outlook.com>
2026-05-04 15:13:35 -0300

    Add: Added refer-a-friend page route on homepage

--- MOBILE/OticaVitturino/app/homepage.jsx
+++ MOBILE/OticaVitturino/app/homepage.jsx
@@ -32,7 +32,7 @@ export default function Homepage() {
                     <Button style={styles.pageButton} textStyle={styles.pageButtonText} title='Agendar Consulta' onPress={() => router.navigate('/booking-page')} />
                     <Button style={styles.pageButton} textStyle={styles.pageButtonText} title='Acompanhar Produção' onPress={() => router.navigate('/production-page')} />
                     <Button style={styles.pageButton} textStyle={styles.pageButtonText} title='Registrar Ocorrência/Reclamação' onPress={() => router.navigate('/incident-history')} />
-                    <Button style={styles.pageButton} textStyle={styles.pageButtonText} title='Indicar Aplicativo' onPress={() => router.navigate('')} />
+                    <Button style={styles.pageButton} textStyle={styles.pageButtonText} title='Indicar Aplicativo' onPress={() => router.navigate('/refer-a-friend')} />
                 </View>
 
             </View>
