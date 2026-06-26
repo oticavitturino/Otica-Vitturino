ca72eb624765fd66c9c36dc01d1131d8f0791e9c
caiovtech <caiovtech@outlook.com>
2026-06-26 16:31:40 -0300

    Fix: Fixed the fetch path on user registration function

--- WEB/FRONT/src/pages/user-management/user-management.jsx
+++ WEB/FRONT/src/pages/user-management/user-management.jsx
@@ -32,7 +32,7 @@ function User_Management() {
     event.preventDefault();
 
     try {
-      const response = await fetch('/auth/register', {
+      const response = await fetch('http://localhost:8080/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
